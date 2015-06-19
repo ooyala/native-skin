3425932314edb93997ba0957f0d61e8eec9104dd
Matthew Chen <zchen@ooyala.com>
2015-06-19 15:54:25 -0700

    [PBA-2036] decouple start screen positions, using absolute values.

--- sdk/OoyalaSkinSDK/StartScreen.js
+++ sdk/OoyalaSkinSDK/StartScreen.js
@@ -15,6 +15,7 @@ var {
 } = Constants;
 
 var ICONS = require('./constants').ICONS;
+var marginValue = 10;
 
 var StartScreen = React.createClass({
   propTypes: {
@@ -23,6 +24,8 @@ var StartScreen = React.createClass({
     description: React.PropTypes.string,
     promoUrl: React.PropTypes.string,
     onPress: React.PropTypes.func,
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
   },
 
   handleClick: function() {
@@ -31,29 +34,44 @@ var StartScreen = React.createClass({
 
   // Gets the play button based on the current config settings
   getPlayButton: function() {
+    if(this.props.config.showPlayButton) {
+      var playButtonLocation;
+      var fontsize = (this.props.height + this.props.width) * 0.1;
+      switch (this.props.config.playButtonPosition) {
+        case "center":
+          playButtonLocation = {
+            position: 'absolute', 
+            top: (this.props.height - fontsize) / 2, 
+            left: (this.props.width - fontsize) / 2};
+          break;
+        case "bottomLeft":
+          playButtonLocation = styles.SW;
+          break;
+        default:
+          throw("Invalid play button location " + this.props.config.playButtonPosition);
+          return null;
+      }
 
-    var playButtonLocation;
-    switch (this.props.config.playButtonPosition) {
-      case "center":
-        playButtonLocation = styles.playButtonCenter;
-        break;
-      case "bottomLeft":
-        playButtonLocation = styles.playButtonSW;
-        break;
-      default:
-        throw("Invalid play button location " + this.props.config.playButtonPosition);
+      var playButtonStyle = {
+        fontSize: fontsize,
+        textAlign: 'center',
+        color: 'white',
+        fontFamily: 'fontawesome',
+        margin: 10
+      };
+
+      return (
+        <View style={playButtonLocation}>
+          <TouchableHighlight
+            onPress={this.handleClick}
+            underlayColor="transparent"
+            activeOpacity={0.5}>
+            <Text style={playButtonStyle}>{ICONS.PLAY}</Text>
+          </TouchableHighlight>
+        </View>
+      );
     }
-    
-    return (
-      <View style={playButtonLocation}>
-        <TouchableHighlight
-          onPress={this.handleClick}
-          underlayColor="transparent"
-          activeOpacity={0.5}>
-          <Text style={styles.playButton}>{ICONS.PLAY}</Text>
-        </TouchableHighlight>
-      </View>
-    );
+    return null;
   },
 
   // Gets the infoPanel based on the current config settings
@@ -70,10 +88,10 @@ var StartScreen = React.createClass({
     var infoPanelLocation;
     switch (this.props.config.infoPanelPosition) {
       case "NW":
-        infoPanelLocation = styles.infoPanelNW;
+        infoPanelLocation = styles.NW;
         break;
       case "SW":
-        infoPanelLocation = styles.infoPanelSW;
+        infoPanelLocation = styles.SW;
         break;
       default:
         throw("Invalid infoPanel location " + this.props.config.infoPanelPosition);
@@ -87,80 +105,53 @@ var StartScreen = React.createClass({
     );
   },
 
-  render: function() {
-    var fullscreenPromoImage = (this.props.config.mode == 'default');
-    
-    var playButton;
-    if(this.props.config.showPlayButton) {
-      playButton = this.getPlayButton();
-    }
-    var infoPanel = this.getInfoPanel();
+  getPromoImage: function() {
+    if (this.props.config.showPromo && this.props.promoUrl) {
+      var fullscreen = (this.props.config.mode == 'default');
 
-    var waterMarkImageLocation = styles.waterMarkImageSE;
-    var waterMarkImage = (
+      return (
+        <Image 
+          source={{uri: this.props.promoUrl}}
+          style={fullscreen ? styles.container : styles.promoImageSmall}
+          resizeMode={Image.resizeMode.contain}>
+        </Image>
+      );
+    }
+    
+    return null;
+  },
+
+  getWaterMark: function () {
+    var waterMarkImageLocation = styles.SE;
+    return (
       <Image style={[styles.waterMarkImage, waterMarkImageLocation]}
         source={{uri: IMG_URLS.OOYALA_LOGO}} 
         resizeMode={Image.resizeMode.contain}>
       </Image>
-      );
+    );
+  },
 
-    var promoUrl;
-    if(this.props.config.showPromo) {
-      promoUrl = this.props.promoUrl;
-    }
+  render: function() {  
+    var promoImage = this.getPromoImage();
+    var playButton = this.getPlayButton();
+    var infoPanel = this.getInfoPanel();
+    var waterMarkImage = this.getWaterMark();
     
-    if (fullscreenPromoImage) {   
-      return (
-        <Image 
-          source={{uri: promoUrl}}
-          style={styles.container}
-          resizeMode={Image.resizeMode.contain}>
-          {infoPanel}
-          {playButton}
-          {waterMarkImage}
-        </Image>);
-    } else {
-      var promoImage = (
-        <Image 
-          source={{uri: promoUrl}}
-          style={styles.promoImageSmall}
-          resizeMode={Image.resizeMode.contain}>
-        </Image>
-      );
-      return (
-        <View style={styles.container}>
-          {playButton}
-          {promoImage}
-          {infoPanel}
-          {waterMarkImage}
-        </View>
-      );
-    }
-  }
+    return (
+      <View style={styles.container}>
+        {promoImage}
+        {waterMarkImage}
+        {infoPanel}
+        {playButton}
+      </View>
+    );
+  },
 });
 
+var padding = 10;
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start'
-  },
-
-  promoImageSmall: {
-    width: 180,
-    height: 90,
-    margin: 20,
-  },
-
-  infoPanelNW: {
-    flexDirection: 'column',
-    backgroundColor: 'transparent',
-  },
-
-  infoPanelSW: {
-    position: 'absolute',    
-    bottom: 0,
-    left: 0
   },
 
   infoPanelTitle: {
@@ -169,7 +160,8 @@ var styles = StyleSheet.create({
     fontFamily: 'Arial-BoldMT',
     color: 'white',
     marginTop: 20,
-    marginLeft: 10
+    marginLeft: 10,
+    marginRight: 10
   },
 
   infoPanelDescription: {
@@ -177,56 +169,50 @@ var styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: 'ArialMT',
     color: 'white',
-    margin: 10
-  },
-
-  playButtonCenter: {
-    flex: 1,
-    flexDirection: 'row',
-    alignSelf: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-
-  playButtonSE: {
-    position: 'absolute',    
-    bottom: 0,
-    right: 0
-  },
-
-  playButtonSW: {
-    position: 'absolute',    
-    bottom: 0,
-    left: 0
-  },
-
-  playButton: {
-    fontSize: 40,
-    textAlign: 'center',
-    color: 'white',
-    fontFamily: 'fontawesome',
-    margin: 10
-  },
-
-  waterMarkImageSW: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-  },
-
-  waterMarkImageSE: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
   },
 
   waterMarkImage: {
     width:160,
     height: 24,
-    alignItems: 'center',
     backgroundColor: 'transparent',
     margin: 10
-  }
+  },
+
+  promoImageSmall: {
+    position: 'absolute',
+    width: 180,
+    height: 90,
+    margin: 20,
+    left: 0,
+    bottom: 50,
+  },
+
+  SW: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+
+  SE: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+
+  NW: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+
+  NE: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
 });
 
 module.exports = StartScreen;
--- sdk/OoyalaSkinSDK/ooyalaSkin.js
+++ sdk/OoyalaSkinSDK/ooyalaSkin.js
@@ -203,6 +203,8 @@ var OoyalaSkin = React.createClass({
         title={this.state.title}
         description={this.state.description}
         promoUrl={this.state.promoUrl}
+        width={this.state.width}
+        height={this.state.height}
         onPress={(name) => this.handlePress(name)}/>
     );
   },
