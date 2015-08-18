15d38f00800333c5f3c89a59f7246d859c0cc210
Daniel Kao <dkao@ooyala.com>
2015-08-18 15:28:19 -0700

    fix volume icon to use the correct icon name.

--- sdk/OoyalaSkinSDK/controlBar.js
+++ sdk/OoyalaSkinSDK/controlBar.js
@@ -104,7 +104,7 @@ var ControlBar = React.createClass({
       volume: {
         onPress: this.onVolumePress,
         style: this.state.showVolume ? [styles.icon, styles.iconHighlighted, this.props.config.controlBar.iconStyle] : [styles.icon, this.props.config.controlBar.iconStyle],
-        icon: this.props.config.icons.volumeDown,
+        icon: this.props.config.icons.volume,
         showVolume: this.state.showVolume,
         scrubberStyle: styles.volumeSlider
       },
