6c9685873371aa4cf3da0da60976991b174709cd
Daniel Kao <dkao@ooyala.com>
2015-08-20 14:47:16 -0700

    add testcases to responsive design manager

--- sdk/OoyalaSkinSDK/responsiveDesignManager.js
+++ sdk/OoyalaSkinSDK/responsiveDesignManager.js
@@ -17,7 +17,6 @@ var ResponsiveDesignManager = {
    *
    * @param args: {
    *  width,
-   *  height,
    *  baseSize,
    *  threshold[],
    *  multiplier[],
@@ -26,7 +25,7 @@ var ResponsiveDesignManager = {
    * }
    */
   makeResponsive: function(args) {
-    // Between which two array threshold elemnts is the current size?
+    // Between which two array threshold elements is the current size?
     var size = 0;
     for(var i in args.threshold) {
       if(args.width > args.threshold[i]) {
@@ -35,20 +34,182 @@ var ResponsiveDesignManager = {
     }
 
     var responsive = {};
-
+    // If the responsive using a multiplier at each size.
     if(args.multiplier) {
+      // Throw an error if the threshold and values are not provided correctly.
+      if(args.multiplier.length != args.threshold.length + 1) {
+        throw new Error("Multiplier array needs to be one element greater than threshold array.");
+      }
       responsive.value = args.baseSize * args.multiplier[size];
     }
+    // If the responsive using specific values at each size.
     else if(args.values) {
+      // Throw an error if the threshold and values are not provided correctly.
+      if(args.values.length != args.threshold.length + 1) {
+        throw new Error("Values array needs to be one element greater than threshold array.");
+      }
       responsive.value = args.values[size];
     }
+    // If no values or multipliers are given
+    else {
+      throw new Error("No responsive methods given.");
+    }
 
+    // If a style is specified to return.
     if(args.style && responsive.value) {
       responsive.style = {};
       responsive.style[args.style] = responsive.value;
     }
 
     return responsive;
+  },
+
+  /**
+   * Contains testcases
+   */
+  TestSuite: {
+
+    Assert: function() {
+      var b = arguments[0];
+      if( ! b ) {
+        throw new Error( 'ASSERTION FAILED: ' + JSON.stringify(arguments) );
+      }
+    },
+    AssertStrictEquals: function() {
+      var o1 = arguments[0];
+      var o2 = arguments[1];
+      if( o1 !== o2 ) {
+        var errorMessage = 'ASSERTION FAILED: ' + JSON.stringify(o1) + ' !== ' + JSON.stringify(o2);
+        if( arguments.length > 2 ) {
+          errorMessage += " (" + JSON.stringify(Array.prototype.slice.call(arguments, 2)) + ")";
+        }
+        throw new Error( errorMessage );
+      }
+    },
+
+    TestResponsive_sizing: function() {
+      var responsiveArgs = {
+        width: 500,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 200, responsive );
+
+      responsiveArgs.width = 100;
+      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 100, responsive );
+
+      responsiveArgs.width = 200;
+      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 100, responsive );
+
+      responsiveArgs.width = 800;
+      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 200, responsive );
+
+      responsiveArgs.width = 900;
+      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 300, responsive );
+
+      responsiveArgs.width = 500;
+      responsiveArgs.multiplier = null;
+      responsiveArgs.values = [200,300,400];
+      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 300, responsive );
+    },
+
+    TestResponsive_invalid: function() {
+      var responsiveArgs = {
+        width: 500,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1]
+      };
+      var responsive = {};
+
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+
+      responsiveArgs.threshold = null;
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+
+      responsiveArgs.multiplier = [1,2,3,4,5];
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+
+      responsiveArgs.multiplier = null;
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+
+      responsiveArgs.multiplier = 1;
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+
+      responsiveArgs.threshold = [200, 800];
+      responsiveArgs.values = [200,500];
+      try {
+        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+        this.Assert(false);
+      }
+      catch (error) {
+        this.Assert(true);
+      }
+    },
+
+    TestResponsive_style: function() {
+      var responsiveArgs = {
+        width: 500,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3],
+        style: 'opacity'
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.style.opacity, 200, responsive );
+    },
+
+    Run: function() {
+      var keys = Object.keys( this ).sort();
+      for( var i = 0; i < keys.length; ++i ) {
+        var k = keys[i];
+        var isFunction = typeof(this[k]) == "function";
+        var isTest = k.indexOf("Test") == 0;
+        if( isFunction && isTest ) {
+          console.log( "+++", k );
+          this[k]();
+          console.log( "---",  k, "PASS!" );
+        }
+      }
+      console.log( "ran", keys.length, "tests." );
+    }
   }
 
 };
