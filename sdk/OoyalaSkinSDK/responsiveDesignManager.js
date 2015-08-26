29d9a6575fc07f793ea1fee9a63908e7445c29d9
Daniel Kao <dkao@ooyala.com>
2015-08-25 17:19:06 -0700

    [PBA 2240] add helper functions, improve testcases, etc

--- sdk/OoyalaSkinSDK/responsiveDesignManager.js
+++ sdk/OoyalaSkinSDK/responsiveDesignManager.js
@@ -6,14 +6,32 @@
 
 var ResponsiveDesignManager = {
 
-  // Default threshold
+  // Default threshold and multiplier
   threshold: [320, 860],
   multiplier: [0.8, 1, 1.2],
 
   /**
-   * makeResponsive takes an object that has the width and height of the
-   * player and returns an object that has it's current state, a rendered
-   * style, and other data for responsiveness.
+   * getSize takes the width and the threshold and returns an integer
+   * that represents which threshold the player width currently lies in.
+   *
+   * @param width
+   * @param threshold
+   * @returns {number}
+   */
+  getSize: function(width, threshold) {
+    var threshold = (threshold) ? threshold : this.threshold;
+    for(var i = 0; i < threshold.length; i++) {
+      if(width <= threshold[i]) {
+        return parseInt(i);
+      }
+    }
+    return threshold.length;
+  },
+
+  /**
+   * makeResponsive takes an object that has the width of the player and
+   * returns an object that has it's current state, a rendered style,
+   * and other data for responsiveness.
    *
    * Priority:
    *  values,
@@ -34,55 +52,19 @@ var ResponsiveDesignManager = {
    * }
    */
   makeResponsive: function(args) {
-    // Between which two array threshold elements is the current size?
-    var size = 0;
-    var threshold;
-
-    // See if threshold is provided or not
-    if(args.threshold) {
-      threshold = args.threshold;
-    }
-    else {
-      threshold = this.threshold;
-    }
-
-    for(var i in threshold) {
-      if(args.width > threshold[i]) {
-        size = parseInt(i) + 1;
-      }
-    }
-
     var responsive = {};
-    // If the responsive using specific values at each size.
+
+    // If the method of responsive manipulation uses specific values between each threshold.
     if(args.values) {
-      // Throw an error if the threshold and values are not provided correctly.
-      if(args.values.length != threshold.length + 1) {
-        throw new Error("Values array needs to be one element greater than threshold array.");
-      }
-      responsive.value = args.values[size];
+      responsive.value = this.makeResponsiveValues(args.width, args.threshold, args.values)
     }
-    // If the responsive using a multiplier at each size.
+    // If the method of responsive manipulation uses a multiplier on the baseSize between each threshold.
     else if(args.baseSize) {
-      var multiplier;
-      // See if multiplier is provided or not
-      if(args.multiplier) {
-        multiplier = args.multiplier;
-      }
-      else {
-        multiplier = this.multiplier;
-      }
-      // Throw an error if the threshold and values are not provided correctly.
-      if(multiplier.length != threshold.length + 1) {
-        throw new Error("Multiplier array needs to be one element greater than threshold array.");
-      }
-      if(!args.baseSize) {
-        throw new Error("baseSize must me given when using multiplier");
-      }
-      responsive.value = args.baseSize * multiplier[size];
+      responsive.value = this.makeResponsiveMultiplier(args.width, args.threshold, args.multiplier, args.baseSize);
     }
     // If no values or multipliers are given
     else {
-      throw new Error("No responsive methods given.");
+      return;
     }
 
     // If a style is specified to return.
@@ -92,6 +74,38 @@ var ResponsiveDesignManager = {
     }
 
     return responsive;
+  },
+
+  /**
+   * makeResponsiveMultiplier calculates the responsive value by multiplying a
+   * constant to the current size of an element based on how big the screen is.
+   *
+   * @param width
+   * @param threshold
+   * @param multiplier
+   * @param baseSize
+   * @returns {number}
+   */
+  makeResponsiveMultiplier: function(width, threshold, multiplier, baseSize) {
+    var size = this.getSize(width, threshold);
+    var multiple = (multiplier) ? multiplier : this.multiplier;
+
+    return baseSize * multiple[size];
+  },
+
+  /**
+   * makeResponsiveValues calculates the responsive value by taking the value from
+   * an array that specifies specific values at each threshold.
+   *
+   * @param width
+   * @param threshold
+   * @param values
+   * @returns {number}
+   */
+  makeResponsiveValues: function(width, threshold, values) {
+    var size = this.getSize(width, threshold);
+
+    return values[size];
   },
 
   /**
@@ -117,7 +131,7 @@ var ResponsiveDesignManager = {
       }
     },
 
-    TestResponsive_sizing: function() {
+    TestResponsive_multiplier_medium: function() {
       var responsiveArgs = {
         width: 500,
         baseSize: 100,
@@ -125,103 +139,90 @@ var ResponsiveDesignManager = {
         multiplier: [1, 2, 3]
       };
       var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 200, responsive );
-
-      responsiveArgs.width = 100;
-      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 100, responsive );
-
-      responsiveArgs.width = 200;
-      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 100, responsive );
-
-      responsiveArgs.width = 800;
-      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 200, responsive );
-
-      responsiveArgs.width = 900;
-      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 300, responsive );
-
-      responsiveArgs.width = 500;
-      responsiveArgs.multiplier = null;
-      responsiveArgs.values = [200,300,400];
-      responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-      this.AssertStrictEquals( responsive.value, 300, responsive );
+      this.AssertStrictEquals(responsive.value, 200, responsive);
     },
 
-    TestResponsive_invalid: function() {
+    TestResponsive_multiplier_medium_default: function() {
+      var responsiveArgs = {
+        width: 500,
+        baseSize: 100
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals(responsive.value, 100, responsive);
+    },
+
+    TestResponsive_multiplier_small: function() {
+      var responsiveArgs = {
+        width: 100,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals(responsive.value, 100, responsive);
+    },
+
+    TestResponsive_multiplier_small_border: function() {
+      var responsiveArgs = {
+        width: 200,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals(responsive.value, 100, responsive);
+    },
+
+    TestResponsive_multiplier_large_border: function() {
+      var responsiveArgs = {
+        width: 800,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals(responsive.value, 200, responsive);
+    },
+
+    TestResponsive_multiplier_large: function() {
+      var responsiveArgs = {
+        width: 900,
+        baseSize: 100,
+        threshold: [200, 800],
+        multiplier: [1, 2, 3]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals(responsive.value, 300, responsive);
+    },
+
+    TestResponsive_values_medium: function() {
       var responsiveArgs = {
         width: 500,
         baseSize: 100,
         threshold: [200, 800],
-        multiplier: [1]
+        values: [200,300,400]
       };
-      var responsive = {};
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, 300, responsive );
+    },
 
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
+    TestResponsive_values_undefined: function() {
+      var responsiveArgs = {
+        width: 500,
+        baseSize: 100,
+        threshold: [200, 800],
+        values: [200]
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive.value, undefined, responsive );
+    },
 
-      responsiveArgs.baseSize = null;
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
-
-      responsiveArgs.baseSize = 100;
-      responsiveArgs.threshold = null;
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
-
-      responsiveArgs.multiplier = [1,2,3,4,5];
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
-
-      responsiveArgs.multiplier = null;
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
-
-      responsiveArgs.multiplier = 1;
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
-
-      responsiveArgs.threshold = [200, 800];
-      responsiveArgs.values = [200,500];
-      try {
-        responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
-        this.Assert(false);
-      }
-      catch (error) {
-        this.Assert(true);
-      }
+    TestResponsive_undefined: function() {
+      var responsiveArgs = {
+        width: 500
+      };
+      var responsive = ResponsiveDesignManager.makeResponsive(responsiveArgs);
+      this.AssertStrictEquals( responsive, undefined, responsive );
     },
 
     TestResponsive_style: function() {
