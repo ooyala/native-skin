fa9251749b212c294174f81fbbe616fa63f2940a
Unnath Kumar <ukumar@ooyala.com>
2015-11-03 13:20:35 -0800

    start screen implementation for title, description and promo.

--- sdk/OoyalaSkinSDK/index.android.js
+++ sdk/OoyalaSkinSDK/index.android.js
@@ -113,7 +113,8 @@ var OoyalaSkin = React.createClass({
     var fontSize = 32;
     switch (this.state.screenType) {
       case SCREEN_TYPES.START_SCREEN: 
-        return this.renderStartScreen(); 
+          return OoyalaSkinCoreInstance.renderStartScreen(); 
+        //this.renderStartScreen(); 
         break;
       // case SCREEN_TYPES.END_SCREEN:   return this._renderEndScreen();   break;
       case SCREEN_TYPES.LOADING_SCREEN: 
