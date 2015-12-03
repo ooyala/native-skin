99b7ecbcea4b2eff4f7caf0ad6e080e053deb221
Unnath Kumar <ukumar@ooyala.com>
2015-12-03 12:51:37 -0800

    PBA-2169 full screen functionality

--- sdk/android/sdk/src/main/java/com/ooyala/android/ooyalaskinsdk/OoyalaSkinLayoutController.java
+++ sdk/android/sdk/src/main/java/com/ooyala/android/ooyalaskinsdk/OoyalaSkinLayoutController.java
@@ -87,7 +87,7 @@ public class OoyalaSkinLayoutController extends ReactContextBaseJavaModule imple
                       | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                       | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                       | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                      | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                      | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
     }
     else
