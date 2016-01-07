1f5ae2410bac3eba8f3e21406c03bacf4a213096
Matthew Chen <zchen@ooyala.com>
2016-01-07 15:20:48 -0800

    [PBA-3019] expose closedcaptionsview

--- sdk/android/sdk/src/main/java/com/ooyala/android/ooyalaskinsdk/ClosedCaptionsViewManager.java
+++ sdk/android/sdk/src/main/java/com/ooyala/android/ooyalaskinsdk/ClosedCaptionsViewManager.java
@@ -4,17 +4,13 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.views.text.ReactTextView;
-import com.ooyala.android.captions.ClosedCaptionsView;
-import com.ooyala.android.captions.ClosedCaptionsStyle;
-import com.ooyala.android.item.Caption;
 
 /**
  * Created by zchen on 12/22/15.
  */
 public class ClosedCaptionsViewManager extends SimpleViewManager<ClosedCaptionsView> {
   public static final String REACT_CLASS = "RCTClosedCaptionsView";
-  private ClosedCaptionsStyle style;
+//  private ClosedCaptionsStyle style;
 
   @Override
   public String getName() {
@@ -24,10 +20,6 @@ public class ClosedCaptionsViewManager extends SimpleViewManager<ClosedCaptionsV
   @Override
   public ClosedCaptionsView createViewInstance(ThemedReactContext context) {
     ClosedCaptionsView v = new ClosedCaptionsView(context);
-    if (style == null) {
-      style = new ClosedCaptionsStyle(context);
-    }
-    v.setStyle(style);
     return v;
   }
 
@@ -35,18 +27,8 @@ public class ClosedCaptionsViewManager extends SimpleViewManager<ClosedCaptionsV
   public void setCaption(ClosedCaptionsView view, ReadableMap caption) {
     Double begin = caption.getDouble("begin");
     Double end = caption.getDouble("end");
+    Double width = caption.getDouble("width");
     String text = caption.getString("text");
-    Caption c = new Caption(begin, end, text);
-    view.setCaption(c);
-  }
-
-  @ReactProp(name = "videoWidth", defaultDouble = 0.0)
-  public void setCaption(ClosedCaptionsView view, double videoWidth) {
-    view.setVideoWidth(videoWidth);
-  }
-
-  @ReactProp(name = "renderToHardwareTextureAndroid")
-  public void setRenderToHardwareTextureAndroid(ClosedCaptionsView view, boolean b) {
-    // do nothing.
+    view.setCaption(begin, end, text, width);
   }
 }
