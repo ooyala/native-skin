0de5b57638e3bc54f6e882bfa03d1a7e96bb70a0
Dmitry Korobov <dkorobov@ooyala.com>
2016-02-11 16:56:08 -0800

    fix formatting

--- sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/ooyalaskinsdk/VolumeViewManager.java
+++ sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/ooyalaskinsdk/VolumeViewManager.java
@@ -12,26 +12,25 @@ import com.facebook.react.uimanager.ThemedReactContext;
  * Created by dkorobov on 2/9/16.
  */
 public class VolumeViewManager extends SimpleViewManager<VolumeView> {
-    public static final String REACT_CLASS = "OOVolumeView";
+  public static final String REACT_CLASS = "OOVolumeView";
 
-    @Override
-    public String getName() {
-        return REACT_CLASS;
-    }
+  @Override
+  public String getName() {
+    return REACT_CLASS;
+  }
 
-    @Override
-    protected VolumeView createViewInstance(ThemedReactContext context) {
-        return new VolumeView(context);
-    }
+  @Override
+  protected VolumeView createViewInstance(ThemedReactContext context) {
+    return new VolumeView(context);
+  }
 
-    @ReactProp(name = "progressColor")
-    public void setProgressColor(VolumeView view, String progressColor) {
-        view.getProgressDrawable().setColorFilter(Color.parseColor(progressColor), PorterDuff.Mode.MULTIPLY);
-    }
+  @ReactProp(name = "progressColor")
+  public void setProgressColor(VolumeView view, String progressColor) {
+    view.getProgressDrawable().setColorFilter(Color.parseColor(progressColor), PorterDuff.Mode.MULTIPLY);
+  }
 
-    @ReactProp(name = "thumbColor")
-    public void setThumbColor(VolumeView view, String thumbColor) {
-        view.getThumb().setColorFilter(Color.parseColor(thumbColor), PorterDuff.Mode.SRC_IN);
-    }
-
+  @ReactProp(name = "thumbColor")
+  public void setThumbColor(VolumeView view, String thumbColor) {
+    view.getThumb().setColorFilter(Color.parseColor(thumbColor), PorterDuff.Mode.SRC_IN);
+  }
 }
