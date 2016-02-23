298b3967da7714928404da1a13e8553490c2d6c5
Michael Len <michael.len@ooyala.com>
2016-02-23 11:34:51 -0800

    Fix OoyalaSkinLayoutController to point to new BuildConfig

--- sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/skin/OoyalaSkinLayoutController.java
+++ sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/skin/OoyalaSkinLayoutController.java
@@ -21,7 +21,7 @@ import com.ooyala.android.OoyalaPlayer;
 import com.ooyala.android.OoyalaPlayerLayout;
 import com.ooyala.android.discovery.DiscoveryManager;
 import com.ooyala.android.discovery.DiscoveryOptions;
-import com.ooyala.android.ooyalaskinsdk.BuildConfig;
+import com.ooyala.android.skin.BuildConfig;
 import com.ooyala.android.player.FCCTVRatingUI;
 import com.ooyala.android.skin.configuration.SkinOptions;
 import com.ooyala.android.skin.util.ReactUtil;
