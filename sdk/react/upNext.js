2d1f496dc2b36bbcf0970550fba03bcc7e5111ba
sveravelli <sveeravelli@ooyala.com>
2016-03-01 15:54:53 -0800

    Changed upNext play button from "h" to Icon.

--- sdk/react/upNext.js
+++ sdk/react/upNext.js
@@ -106,8 +106,9 @@ var UpNext = React.createClass({
           style={[styles.thumbnail, {width: upNextWidth}]} >
           <TouchableHighlight style={[styles.thumbnail, {width: upNextWidth}]}
             onPress={this.clickUpNext}>
-            <Text style={[{fontFamily: this.props.config.icons.play.fontFamilyName, color: "white"}, styles.countdownText]}>{this.props.config.icons.play.fontString}</Text>
+            <Text style={[{fontFamily: this.props.config.icons.play.fontFamilyName, color: "white"}, styles.countdownText]}></Text>
           </TouchableHighlight>
+          <Text style={styles.icon}>{this.props.config.icons.play.fontString}</Text>
         </Image>
       );
 
