131de730ac0819e24ff24e02e046fd2c7118dce7
Michael Len <michael.len@ooyala.com>
2016-03-14 16:30:19 -0700

    [PBA-3482] [PBA-3446] [PBA-3508] Fix Watermark to respect FlexBox
    No more Absolute.  more Flex

--- sdk/react/widgets/videoWaterMark.js
+++ sdk/react/widgets/videoWaterMark.js
@@ -19,8 +19,6 @@ var {
 
 var logo = React.createClass({
   propTypes: {
-    frameWidth: React.PropTypes.number,
-    frameHeight: React.PropTypes.number,
     buttonWidth: React.PropTypes.number,
     buttonHeight: React.PropTypes.number,
     waterMarkName: React.PropTypes.string,
@@ -35,54 +33,20 @@ getInitialState: function() {
     };
   },
 
- componentDidUpdate: function(prevProps, prevState) {
-    if(prevProps.width != this.props.width && this.props.isShow) {
-      this.state.height.setValue(ResponsiveDesignManager.makeResponsiveMultiplier(this.props.width, UI_SIZES.CONTROLBAR_HEIGHT));
-    }
-    if(prevProps.isShow != this.props.isShow ) {
-      this.state.opacity.setValue(1);
-      this.state.height.setValue(this.props.isShow? 0 : ResponsiveDesignManager.makeResponsiveMultiplier(this.props.width, UI_SIZES.CONTROLBAR_HEIGHT));
-      Animated.parallel([
-        Animated.timing(
-          this.state.opacity,
-          {
-            toValue: this.props.isShow ? 1 : UI_SIZES.CONTROLBAR_HEIGHT,
-            duration: 500,
-            delay: 0
-          }),
-        Animated.timing(
-          this.state.height,
-          {
-            toValue: this.props.isShow ? ResponsiveDesignManager.makeResponsiveMultiplier(this.props.width, UI_SIZES.CONTROLBAR_HEIGHT) : this.props.buttonHeight,
-            duration: 500,
-            delay: 0
-          })
-      ]).start();
-    }
-  },
-
 renderLogo: function(){
-  var widthStyle = {width:this.props.buttonWidth, opacity:this.state.opacity};
-  var topOffset = Math.round((this.props.frameHeight - this.props.buttonHeight) * 0.15);
-  var leftOffset = Math.round((this.props.frameWidth - this.props.buttonWidth) * 0.03);
   var waterMarkName = this.props.waterMarkName;
-  positionStyle = {
-    position: 'absolute', bottom: topOffset, right: leftOffset
-  };  
   var sizeStyle = {width: this.props.buttonWidth, height: this.props.buttonHeight};
-      return (
-        <Animated.View style={[[positionStyle], widthStyle, {"height": this.state.height}]}>
-          <Animated.View style={[styles.buttonArea, sizeStyle]}>
-            <Image
-              style={sizeStyle}
-              source={{uri: waterMarkName}}/>
-          </Animated.View>
-        </Animated.View>
+  return (
+        <View style={[styles.watermarkContainer]}>
+          <Image
+            style={sizeStyle}
+            source={{uri: waterMarkName}}/>
+        </View>
       );
   },
 
-render: function() {
-return this.renderLogo();
-}
+  render: function() {
+    return this.renderLogo();
+  }
 });
 module.exports = logo;
