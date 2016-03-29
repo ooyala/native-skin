f1a6367e4d82af594a81126b64702cbcac0df85c
Dmitry Korobov <dkorobov@ooyala.com>
2016-03-28 18:09:15 -0700

    better fix for initial video loading in Skin Playback

--- sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/skin/OoyalaSkinLayout.java
+++ sdk/android/ooyalaSkinSDK/src/main/java/com/ooyala/android/skin/OoyalaSkinLayout.java
@@ -2,7 +2,6 @@ package com.ooyala.android.skin;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
