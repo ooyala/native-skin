9d51cfef0c4cba9a60c7fbea35ad184740ed69ff
Matthew Chen <zchen@ooyala.com>
2016-06-20 16:32:00 -0700

    [PBA-4135] fix cc style.

--- sdk/react/panels/videoView.js
+++ sdk/react/panels/videoView.js
@@ -166,11 +166,17 @@ var VideoView = React.createClass({
 
   _renderClosedCaptions: function() {
     if (this.props.caption) {
+      var ccStyle = null;
+      if(this.props.platform == Constants.PLATFORMS.ANDROID) {
+        ccStyle = {fontSize: this.props.captionStyles.textSize, color:this.props.captionStyles.textColor,
+      backgroundColor:this.props.captionStyles.backgroundColor};
+      }
+      
       return (
         <View 
           style={panelStyles.closedCaptionsContainer}
           onTouchEnd={(event) => this.props.handlers.handleVideoTouch(event)}>
-          <Text style={panelStyles.closedCaptions}>
+          <Text style={[panelStyles.closedCaptions, ccStyle]}>
             {this.props.caption}
           </Text>
         </View>
