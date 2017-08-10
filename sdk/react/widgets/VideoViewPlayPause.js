ebc248ba8e9db098418778ebcc41aae3ae92cfde
alangarcia001 <agarcia@ooyala.com>
2017-08-10 17:49:05 -0500

    PLAYER-1391 PlayPause reader

--- sdk/react/widgets/VideoViewPlayPause.js
+++ sdk/react/widgets/VideoViewPlayPause.js
@@ -149,7 +149,7 @@ var VideoViewPlayPause = React.createClass({
       
       <View accessible={true} accessibilityLabel={this.state.play.animationOpacity._value ? "play" : "pause"} accessibilityComponentType="button" 
          style={[styles.buttonTextStyle, size]}>
-        <Animated.Text accessible={false} //Animated text is not accessible to read the label of View
+        <Animated.Text accessible={false} 
           style={[styles.buttonTextStyle, fontStyle, buttonColor, this.props.buttonStyle, animate, opacity]}>
           {this.props.icons[name].icon}
         </Animated.Text>
