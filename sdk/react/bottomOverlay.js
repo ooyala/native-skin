f02385b644868f12ed57e177e57e9a7880e62087
Ivan Sakharovskii <ivan_sakharovskii@epam.com>
2018-03-13 01:17:41 +0300

    [PLAYER-2937] Remove accessible true for slider

--- sdk/react/bottomOverlay.js
+++ sdk/react/bottomOverlay.js
@@ -204,10 +204,8 @@ If the playhead position has changed, reset the cachedPlayhead to -1 so that it 
       var percentLabel = parseInt(playedPercent * 100, 10) + "%"
       return (
         <View
-          accessible={true}
           testID={VIEW_NAMES.TIME_SEEK_BAR}
           accessibilityLabel={VIEW_NAMES.TIME_SEEK_BAR}
-          accessibilityLabel={percentLabel}
           style={styles.progressBarStyle}>
           {this._renderProgressBar(playedPercent)}
           {this._renderProgressScrubber(!this.props.ad && this.state.touch ? this.touchPercent(this.state.x) : playedPercent)}
