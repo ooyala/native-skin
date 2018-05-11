299efca1f70f746acee0e2a9050d2f19d3b07078
Alina <alina_voronkova@epam.com>
2018-05-11 12:10:27 +0300

    [PLAYER-3408] Multi audio: audio tracks order is the same for all the platforms (#677)

--- sdk/android/skin/src/main/java/com/ooyala/android/skin/OoyalaSkinPlayerObserver.java
+++ sdk/android/skin/src/main/java/com/ooyala/android/skin/OoyalaSkinPlayerObserver.java
@@ -12,10 +12,10 @@ import com.ooyala.android.item.ClosedCaptions;
 import com.ooyala.android.player.exoplayer.multiaudio.AudioTrack;
 import com.ooyala.android.util.DebugMode;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Set;
 
 /**
  * The class that solely listens to the OoyalaPlayer, and responds based on the events
@@ -229,7 +229,7 @@ class OoyalaSkinPlayerObserver implements Observer {
    * Send the notification to Skin if multi audio is enabled or not for the current asset.
    */
   private void bridgeMultiAudioEnabledNotification() {
-    Set<AudioTrack> audioTracks = _player.getAvailableAudioTracks();
+    List<AudioTrack> audioTracks = _player.getAvailableAudioTracks();
     WritableMap params = BridgeMessageBuilder.buildMultiAudioParams(audioTracks);
     _layoutController.sendEvent(OoyalaPlayer.MULTI_AUDIO_ENABLED_NOTIFICATION_NAME, params);
   }
