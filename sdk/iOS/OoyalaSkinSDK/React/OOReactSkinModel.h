85840c015e8481f75b4cd413c77283fb669df152
Max Kupetskii <maksim_kupetskii@epam.com>
2018-08-15 16:46:08 +0300

    [PLAYER-3979 REFACTOR] OOReactSkinModel.h added
    -model for handling skin logic
    -capable to send event to RN
    -has logic for OOoyalaPlayer management
    -declares OOReactSkinModelDelegate for setting it to RN Modules

---
+++ sdk/iOS/OoyalaSkinSDK/React/OOReactSkinModel.h
@@ -0,0 +1,66 @@
+//
+//  OOReactSkinModel.h
+//  OoyalaSkinSDK
+//
+//  Created by Maksim Kupetskii on 8/13/18.
+//  Copyright © 2018 ooyala. All rights reserved.
+//
+
+#import <React/RCTBridgeDelegate.h>
+#import <React/RCTRootView.h>
+#import "OOSkinViewControllerDelegate.h"
+
+@class OOOoyalaPlayer;
+@class OOSkinOptions;
+@class OOClosedCaptionsStyle;
+
+@interface OOReactSkinModel : NSObject<RCTBridgeDelegate>
+
+@property (nonatomic) NSDictionary *skinConfig;
+@property (nonatomic, strong, readwrite) OOClosedCaptionsStyle *closedCaptionsDeviceStyle;
+@property (nonatomic, readonly) CGRect videoViewFrame;
+
+- (instancetype)initWithWithPlayer:(OOOoyalaPlayer *)player
+                       skinOptions:(OOSkinOptions *)skinOptions
+            skinControllerDelegate:(id<OOSkinViewControllerDelegate>)skinControllerDelegate;
+- (RCTRootView*)viewForModuleWithName:(NSString *)moduleName;
+- (void)sendEventWithName:(NSString *)eventName body:(id)body;
+- (void)setIsReactReady:(BOOL)isReactReady;
+- (void)ccStyleChanged:(NSNotification *)notification;
+//- (CGRect)videoViewFrame;
+
+// Note: This is for IMA ad playback only.
+// When IMA ad plays, IMA consumes clicks for learn more, skip, etc and notify ooyala if the click is not consumed.
+// toggle play/pause as if the alice ui is clicked.
+// see: OOSkinPlayerObserver bridgeAdTappedNotification
+- (void)playPauseFromAdTappedNotification;
+- (void)maybeLoadDiscovery:(NSString *)embedCode;
+- (void)setReactViewInteractionEnabled:(BOOL)reactViewInteractionEnabled;
+
+@end
+
+@protocol OOReactSkinModelDelegate <NSObject>
+
+- (void)toggleFullscreen;
+- (void)toggleStereoMode;
+- (void)handlePip;
+- (void)handleIconClick:(NSInteger)index;
+- (void)handlePlayPause;
+- (void)handlePlay;
+- (void)handleRewind;
+- (void)handleSocialShare;
+- (void)handleLearnMore;
+- (void)handleSkip;
+- (void)handleMoreOption;
+- (void)handleUpNextDismiss;
+- (void)handleUpNextClick;
+- (void)handleLanguageSelection:(NSString *)language;
+- (void)handleAudioTrackSelection:(NSString *)audioTrackName;
+- (void)handleOverlay:(NSString *)url;
+- (void)handleTouch:(NSDictionary *)result;
+- (void)handleScrub:(Float64)position;
+- (void)setEmbedCode:(NSString *)embedCode;
+- (void)handleDiscoveryClick:(NSString *)bucketInfo embedCode:(NSString *)embedCode;
+- (void)handleDiscoveryImpress:(NSString *)bucketInfo;
+
+@end
