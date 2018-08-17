06c16fc9264e3aa0c80d93c72d132bd940c23f78
Mark Mizinov <mark_mizinov@epam.com>
2018-08-17 18:52:52 +0300

    [PBA-6154] Added new logic for auto full screen mode with device rotation changes

--- sdk/iOS/OoyalaSkinSDK/ViewControllers/FullscreenStateController/FullscreenStateController.m
+++ sdk/iOS/OoyalaSkinSDK/ViewControllers/FullscreenStateController/FullscreenStateController.m
@@ -11,7 +11,9 @@
 #import "PresentedViewControllerHelper.h"
 
 
-#define FULLSCREEN_ANIMATION_DURATION 0.5
+#pragma mark - Constants
+
+#define FULLSCREEN_ANIMATION_DURATION 0.4
 
 
 @interface FullscreenStateController()
@@ -19,6 +21,7 @@
 @property (nonatomic) UIView *parentView;
 @property (nonatomic) UIView *containerView;
 @property (nonatomic) UIView *videoView;
+@property (nonatomic) CGRect originalVideoViewFrame;
 @property (nonatomic) UIViewController *rootViewController; // Root VC from visible UIWindow
 @property (nonatomic) UIViewController *fullscreenViewController;
 @property (nonatomic) NSOperationQueue *operationQueue; // Queue for fullscreen animation
@@ -52,18 +55,19 @@
 
 #pragma mark - Public functions
 
-- (void)setFullscreen:(BOOL)fullscreen completion:(nullable void (^)())completion {
+- (void)setFullscreen:(BOOL)fullscreen withOrientaionChanges:(BOOL)isOrientaionChanges completion:(nullable void (^)())completion {
   for (NSOperation *operation in self.operationQueue.operations) {
     if (!operation.isExecuting) {
       [operation cancel];
     }
   }
   
-  NSOperation *operation = [[FullscreenStateOperation alloc] initWithFullscreen:fullscreen enterFullscreenBlock:^(void (^animationCompletion)(void)) {
+  NSOperation *operation = [[FullscreenStateOperation alloc] initWithFullscreen:fullscreen
+                                                           enterFullscreenBlock:^(void (^animationCompletion)(void)) {
     if (self.isFullscreen == fullscreen) {
       animationCompletion();
     } else {
-      [self openFullscreenMode:^{
+      [self openFullscreenModeWithOrientaionChanges:isOrientaionChanges completion:^{
         animationCompletion();
       }];
     }
@@ -80,6 +84,17 @@
   [self.operationQueue addOperation:operation];
 }
 
+- (void)viewWillTransition:(BOOL)isAutoFullscreenWithRotatedEnabled {
+  UIWindow *window = [UIApplication sharedApplication].keyWindow;
+  CGRect frameInWindow = [self.videoView.superview convertRect:self.videoView.frame toView:window];
+  
+  if (isAutoFullscreenWithRotatedEnabled) {
+    self.originalVideoViewFrame = frameInWindow;
+  } else {
+    self.originalVideoViewFrame = CGRectZero;
+  }
+}
+
 #pragma mark - Private functions
 
 - (void)configure {
@@ -88,71 +103,78 @@
   self.isOriginalStatusBarHidden = UIApplication.sharedApplication.isStatusBarHidden;
 }
 
-- (void)openFullscreenMode:(nullable void (^)())completion {
-  UIWindow *window = [UIApplication sharedApplication].keyWindow;
-  
+- (void)openFullscreenModeWithOrientaionChanges:(BOOL)isOrientationChanges completion:(nullable void (^)())completion {
+  UIWindow *window = UIApplication.sharedApplication.keyWindow;
+
   // Update original status bar state
-  
+
   if (!self.isFullscreen) {
     self.isOriginalStatusBarHidden = UIApplication.sharedApplication.isStatusBarHidden;
   }
-  
+
   // Save root VC
-  
-  UIInterfaceOrientation orientation = self.rootViewController.preferredInterfaceOrientationForPresentation;
-  NSLog(@"Preffered orientation is: %ldl", (long)orientation);
+
   self.rootViewController = window.rootViewController;
-  
+
   // Store presented view controller
-  
+
   self.presentedViewControllerHelper.rootViewController = window.rootViewController;
-  [_presentedViewControllerHelper findAndStorePresentedViewController];
+  [self.presentedViewControllerHelper findAndStorePresentedViewController];
+  
+  // Save start rect for not orientation fullscreen mode
+
+  CGRect currentVideoViewFrame = [self.videoView.superview convertRect:self.videoView.frame toView:window];
   
   // Remove video view from container
-  
+
   [self.videoView removeFromSuperview];
-  
-  // Configure fullscreen VC
-  
-  self.fullscreenViewController.view.frame = window.bounds;
-  [self.fullscreenViewController.view addSubview:self.videoView];
-  
-  // Convert rect for animation
-  
-  CGRect frameInWindow = [self.parentView convertRect:self.containerView.frame
-                                               toView:window];
-  
-  // Set start rect for video view
-  
-  self.videoView.frame = frameInWindow;
-  
+
   // Add fullscreen VC on window as subview
-  
+
   [window addSubview:self.fullscreenViewController.view];
   [window bringSubviewToFront:self.fullscreenViewController.view];
+
+  // Configure fullscreen VC
+
+  self.fullscreenViewController.view.frame = window.bounds;
+  [self.fullscreenViewController.view addSubview:self.videoView];
+
+  // Set start rect for video view
+
+  if (isOrientationChanges) {
+    self.videoView.frame = self.originalVideoViewFrame;
+  } else {
+    self.videoView.frame = currentVideoViewFrame;
+  }
+  
+  // Perform animation
   
   [UIView animateWithDuration:FULLSCREEN_ANIMATION_DURATION animations:^{
     self.videoView.frame = window.bounds;
   } completion:^(BOOL finished) {
-
+    
     // Dismiss presented VCs
-    
+
     [_presentedViewControllerHelper dismissPresentedViewControllersWithCompletionBlock:^{
-    
+
       // Change root VC
-      
+
       window.rootViewController = self.fullscreenViewController;
-      
+
       // Update current fullscreen state
-      
+
       self.isFullscreen = YES;
-      
+
       // Hide status bar (it needs when UIViewControllerBasedStatusBarAppearance = YES)
-      
+
       UIApplication.sharedApplication.statusBarHidden = YES;
-      
+
+      // Clear data for orientation fullscreen mode
+
+      self.originalVideoViewFrame = CGRectNull;
+
       // Completion
-      
+
       if (completion) {
         completion();
       }
@@ -170,52 +192,60 @@
   // Set stored root view controller
   
   window.rootViewController = self.rootViewController;
+  
+  // Add fullscreen VC on window as subview
+  
+  [window addSubview:self.fullscreenViewController.view];
+  [window bringSubviewToFront:self.fullscreenViewController.view];
 
   // Show presented view controllers
   
   [self.presentedViewControllerHelper presentStoredControllersWithCompletionBlock:^{
-    
+
     // Choose viewController's view that shows parentView
-    
+
     UIView *viewControllersViewToShow;
-    
+
     if (self.presentedViewControllerHelper.presentedViewController) {
       viewControllersViewToShow = self.presentedViewControllerHelper.presentedViewController.view;
-      [window addSubview:self.fullscreenViewController.view];
-      [window bringSubviewToFront:self.fullscreenViewController.view];
     } else {
       viewControllersViewToShow = self.fullscreenViewController.view;
     }
-    
+
     // Convert rect for animation
-    CGRect frameInFullscreenView = [self.parentView convertRect:self.containerView.frame
-                                                         toView:viewControllersViewToShow];
-    
+
+    CGRect videoViewFrameInFullscreenView = [self.parentView convertRect:self.containerView.frame
+                                                                  toView:viewControllersViewToShow];
+
     // Perform animation
-    
+
     [UIView animateWithDuration:FULLSCREEN_ANIMATION_DURATION animations:^{
-      self.videoView.frame = frameInFullscreenView;
+      self.videoView.frame = videoViewFrameInFullscreenView;
     } completion:^(BOOL finished) {
-      
+
       [self.fullscreenViewController.view removeFromSuperview];
       [self.videoView removeFromSuperview];
-      
+
       [self.containerView addSubview:self.videoView];
-      self.videoView.frame = self.parentView.bounds;
-      
+      self.videoView.frame = self.containerView.bounds;
+
       // Update current fullscreen state
-      
+
       self.isFullscreen = NO;
-      
+
+      // Clear data for presented view controller helper
+
+      [self.presentedViewControllerHelper clearData];
+
+      // Clear data for orientation fullscreen mode
+
+      self.originalVideoViewFrame = CGRectNull;
+
       // Completion
-      
+
       if (completion) {
         completion();
       }
-      
-      // Clear data for presented view controller helper
-      
-      [self.presentedViewControllerHelper clearData];
     }];
   }];
 }
