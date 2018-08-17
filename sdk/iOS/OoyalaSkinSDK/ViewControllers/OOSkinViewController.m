b4faa21dfb15f283489437e39760da673fda893f
Max Kupetskii <maksim_kupetskii@epam.com>
2018-08-17 09:19:11 +0300

    [PBA-3979_REFACTOR] Headers comments updated

--- sdk/iOS/OoyalaSkinSDK/ViewControllers/OOSkinViewController.m
+++ sdk/iOS/OoyalaSkinSDK/ViewControllers/OOSkinViewController.m
@@ -3,7 +3,7 @@
 //  OoyalaSkin
 //
 //  Created by Zhihui Chen on 4/16/15.
-//  Copyright (c) 2015 Facebook. All rights reserved.
+//  Copyright (c) 2015 Ooyala. All rights reserved.
 //
 
 #import "OOSkinViewController.h"
