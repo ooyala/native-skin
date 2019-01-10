2220ec1db10d7e77a8fb4209ed7254729277b850
isheftel-ooyala <isheftel@ooyala.com>
2019-01-10 18:45:39 +0300

    [PLAYER-4945] Resizable layout logic refactored

--- sdk/android/skin/src/main/java/com/ooyala/android/skin/OoyalaSkinLayout.java
+++ sdk/android/skin/src/main/java/com/ooyala/android/skin/OoyalaSkinLayout.java
@@ -4,6 +4,7 @@ package com.ooyala.android.skin;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,7 @@ public class OoyalaSkinLayout extends FrameLayout implements View.OnSystemUiVisi
   private boolean multiWindowMode = false;
 
   private int sourceWidth, sourceHeight;
+  private boolean resizableLayout;
 
   public interface FrameChangeCallback {
     void onFrameChangeCallback(int width, int height, int prevWidth, int prevHeight);
@@ -51,6 +53,14 @@ public class OoyalaSkinLayout extends FrameLayout implements View.OnSystemUiVisi
    */
   public OoyalaSkinLayout(Context context, AttributeSet attrs) {
     super(context, attrs);
+    TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+            attrs,
+            R.styleable.OoyalaSkinLayout, 0, 0);
+    try {
+      resizableLayout = typedArray.getBoolean(R.styleable.OoyalaSkinLayout_resizableLayout, false);
+    } finally {
+      typedArray.recycle();
+    }
     createSubViews();
   }
 
@@ -72,7 +82,7 @@ public class OoyalaSkinLayout extends FrameLayout implements View.OnSystemUiVisi
   public void createSubViews() {
     if (playerFrame == null) {
       FrameLayout.LayoutParams frameLP =
-         new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+              new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
       playerFrame = new FrameLayout(this.getContext());
       this.addView(playerFrame, frameLP);
 
@@ -132,27 +142,34 @@ public class OoyalaSkinLayout extends FrameLayout implements View.OnSystemUiVisi
     return this.fullscreen;
   }
 
+  public int getSourceWidth() {
+    return sourceWidth;
+  }
+
+  public int getSourceHeight() {
+    return sourceHeight;
+  }
 
   /**
    * Show/Hide system ui (notification and navigation bar) depending if layout is in fullscreen
    */
   public void toggleSystemUI(boolean fullscreen) {
-    if(fullscreen) {
+    if (fullscreen) {
       if (SDK_INT >= KITKAT) {
         setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION     // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN          // hide status bar
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  // toggle system UI visibility automatically
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION     // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN          // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  // toggle system UI visibility automatically
       } else {
         setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION     // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);        // hide status bar
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION     // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);        // hide status bar
       }
     } else {
       setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
@@ -206,11 +223,15 @@ public class OoyalaSkinLayout extends FrameLayout implements View.OnSystemUiVisi
   @Override
   protected void onConfigurationChanged(Configuration newConfig) {
     super.onConfigurationChanged(newConfig);
-    updateLayoutSize();
+    if (!resizableLayout) {
+      updateLayoutSize();
+    }
   }
 
   @Override
   public void onSystemUiVisibilityChange(int i) {
-    updateLayoutSize();
+    if (!resizableLayout) {
+      updateLayoutSize();
+    }
   }
 }
