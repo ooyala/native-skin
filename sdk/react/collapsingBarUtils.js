7377ceef0691afb93dd165a1552b692a24f0a16e
Max Kupetskii <maksim_kupetskii@epam.com>
2019-01-29 16:43:07 +0300

    [PLAYER-5000] let in for loops

--- sdk/react/collapsingBarUtils.js
+++ sdk/react/collapsingBarUtils.js
@@ -16,20 +16,20 @@ const CollapsingBarUtils = {
   // @return {fit:[items that fit in the barWidth], overflow:[items that did not fit]}.
   // Note: items which do not meet the item spec will be removed and not appear in the results.
   collapse: function(barWidth, orderedItems) {
-    if (isNaN(barWidth) || barWidth === undefined) { 
-      return {fit: orderedItems, overflow: []}; 
+    if (isNaN(barWidth) || barWidth === undefined) {
+      return {fit: orderedItems, overflow: []};
     }
     if (!orderedItems) {
        return [];
     }
     const validItems = orderedItems.filter(item => this._isValid(item));
-    const result = this._collapse(barWidth, validItems);  
+    const result = this._collapse(barWidth, validItems);
     return result;
   },
 
   collapseForAudioOnly: function(orderedItems) {
-    if (!orderedItems) { 
-      return []; 
+    if (!orderedItems) {
+      return [];
     }
     const filteredItems = orderedItems.filter(item => this._isValid(item));
     const result = {fit: [], overflow: filteredItems};
@@ -54,15 +54,15 @@ const CollapsingBarUtils = {
       if (c.minWidth && c.isVisible) return p+c.minWidth;
       return p;
     }, 0);
-    
-    for (const i = orderedItems.length - 1; i >= 0; --i) {
+
+    for (let i = orderedItems.length - 1; i >= 0; --i) {
       const item = orderedItems[i];
       if (this._isOnlyInMoreOptions(item)) {
         usedWidth = this._collapseLastItemMatching(result, item, usedWidth);
       }
     }
 
-    for (const i = orderedItems.length - 1; i >= 0; --i) {
+    for (let i = orderedItems.length - 1; i >= 0; --i) {
       const item = orderedItems[i];
       if (usedWidth > barWidth && this._isCollapsable(item)) {
         usedWidth = this._collapseLastItemMatching(result, item, usedWidth);
@@ -76,9 +76,9 @@ const CollapsingBarUtils = {
   },
 
   _isCollapsable: function(item) {
-    const collapsable = item.location == constants.controlBar && 
-      item.whenDoesNotFit && 
-      item.whenDoesNotFit != constants.keep;
+    const collapsable = item.location == constants.controlBar &&
+                        item.whenDoesNotFit &&
+                        item.whenDoesNotFit != constants.keep;
     return collapsable;
   },
 
