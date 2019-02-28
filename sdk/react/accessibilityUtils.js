3363b38ae9b96fc1e7b73372cd1546a5b5333780
Max Kupetskii <maksim_kupetskii@epam.com>
2019-02-28 16:26:53 +0300

    [PLAYER-4864] AccessibilityUtils to class

--- sdk/react/accessibilityUtils.js
+++ sdk/react/accessibilityUtils.js
@@ -8,9 +8,9 @@ import {
   ACCESSIBILITY_COMMON
 } from './constants'
 
-let AccessibilityUtils = {
+class AccessibilityUtils {
 
-  createAccessibilityLabelForCell: function(cellType, param) {
+  static createAccessibilityLabelForCell(cellType, param) {
     switch (cellType) {
       case CELL_TYPES.MULTI_AUDIO:
         return param + " " + VIEW_ACCESSIBILITY_NAMES.MULTI_AUDIO_CELL;
@@ -21,13 +21,13 @@ let AccessibilityUtils = {
       default:
         return "";
     }
-  },
+  }
 
-  createAccessibilityLabelForSelectedObject: function(selectedObject) {
+  static createAccessibilityLabelForSelectedObject(selectedObject) {
     return ACCESSIBILITY_COMMON.SELECTED + " " + selectedObject;
-  },
+  }
 
-  createAccessibilityAnnouncers: function(announcerType, param) {
+  static createAccessibilityAnnouncers(announcerType, param) {
     switch (announcerType) {
       case ANNOUNCER_TYPES.MOVING:
         return ACCESSIBILITY_ANNOUNCERS.PROGRESS_BAR_MOVING + param + " %";
@@ -36,17 +36,16 @@ let AccessibilityUtils = {
       default:
         return "";
     }
-  },
-
-  createAccessibilityForForwardButton: function(isForward, param, timeUnit) {
-    const baseLabel = isForward ? VIEW_ACCESSIBILITY_NAMES.FORWARD_BUTTON : VIEW_ACCESSIBILITY_NAMES.BACKWARD_BUTTON;
-    return baseLabel + ' ' + param + ' ' + timeUnit;
-  },
-
-  createAccessibilityForPlayPauseButton: function(buttonName) {
-    return buttonName + ' ' + VIEW_ACCESSIBILITY_NAMES.PLAY_PAUSE_BUTTON + ' ' + buttonName;
   }
 
+  static createAccessibilityForForwardButton(isForward, param, timeUnit) {
+    const baseLabel = isForward ? VIEW_ACCESSIBILITY_NAMES.FORWARD_BUTTON : VIEW_ACCESSIBILITY_NAMES.BACKWARD_BUTTON;
+    return baseLabel + ' ' + param + ' ' + timeUnit;
+  }
+
+  static createAccessibilityForPlayPauseButton(buttonName) {
+    return buttonName + ' ' + VIEW_ACCESSIBILITY_NAMES.PLAY_PAUSE_BUTTON + ' ' + buttonName;
+  }
 };
 
 module.exports = AccessibilityUtils;
