a0598da0c6b12562f6e827cf5004d13a3f50fc82
Max Kupetskii <maksim_kupetskii@epam.com>
2019-02-28 12:27:55 +0300

    [PLAYER-4864] castAirPlay.js introduced

---
+++ sdk/react/panels/CastAirPlayScreen.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+    Modal,
+    TouchableHighlight,
+    View,
+    Alert,
+    Text
+} from 'react-native';
+
+import AirPlayView from '../widgets/AirPlayView'
+
+class CastAirPlayScreen extends Component {
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+export default CastAirPlayScreen;
