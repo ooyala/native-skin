2bcbd924fefa9a62491a20db7b96802bb2f9d354
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-03-04 15:30:56 +0200

    [PLAYER-5077] OOPiPViewManager added

---
+++ sdk/iOS/OoyalaSkinSDK/Views/OOPiPViewManager.h
@@ -0,0 +1,17 @@
+//
+//  OOPiPViewManager.h
+//  OoyalaSkinSDK
+//
+//  Created by Oleksandr Sulyma on 3/4/19.
+//  Copyright © 2019 ooyala. All rights reserved.
+//
+
+#import "React/RCTViewManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OOPiPViewManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdk/iOS/OoyalaSkinSDK/Views/OOPiPViewManager.m
@@ -0,0 +1,13 @@
+//
+//  OOPiPViewManager.m
+//  OoyalaSkinSDK
+//
+//  Created by Oleksandr Sulyma on 3/4/19.
+//  Copyright © 2019 ooyala. All rights reserved.
+//
+
+#import "OOPiPViewManager.h"
+
+@implementation OOPiPViewManager
+
+@end
