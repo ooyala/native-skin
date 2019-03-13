dd6b4a0178eddc0d22f3fe1c81b91b52f9359fbf
atopilskii@ooyala.com <atopilskii@ooyala.com>
2019-03-13 13:26:44 +0300

    [PLAYER-5035] Delete unused code.

--- sdk/react/widgets/AirPlayView.js
+++ sdk/react/widgets/AirPlayView.js
@@ -1,14 +1,6 @@
-import PropTypes from 'prop-types';
 import {
   requireNativeComponent,
 } from 'react-native';
-
-const iface = {
-  name: 'AirPlayView',
-  propTypes: {
-    volume: PropTypes.object,
-  }
-};
 
 // React automatically resolves this class from 'OOAirPlayViewManager'
 module.exports = requireNativeComponent('OOAirPlayView', null);
