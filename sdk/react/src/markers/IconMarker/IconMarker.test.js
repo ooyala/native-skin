ec8188272dbd991c63ce604ed608cba58dd93708
Danila Loginov <danila@loginov.rocks>
2019-04-02 13:00:19 +0300

    [PLAYER-5441] Add tests for `onSeek` triggering

--- sdk/react/src/markers/IconMarker/IconMarker.test.js
+++ sdk/react/src/markers/IconMarker/IconMarker.test.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import TestRenderer from 'react-test-renderer';
 
 import IconMarker from './IconMarker';
@@ -28,5 +29,39 @@ describe('IconMarker', () => {
     );
 
     expect(testRenderer.toJSON()).toBeNull();
+  });
+
+  it('triggers `onSeek` callback every time when pressed if `imageUrl` is empty', () => {
+    const onSeekMock = jest.fn();
+    const testRenderer = TestRenderer.create(
+      <IconMarker
+        iconUrl="http://example.com/icon.png"
+        imageUrl={undefined}
+        onSeek={onSeekMock}
+      />,
+    );
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).toBeCalled();
+  });
+
+  it('triggers `onSeek` callback when pressed being expanded', () => {
+    const onSeekMock = jest.fn();
+    const testRenderer = TestRenderer.create(
+      <IconMarker
+        iconUrl="http://example.com/icon.png"
+        imageUrl="http://example.com/image.png"
+        onSeek={onSeekMock}
+      />,
+    );
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).not.toBeCalled();
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).toBeCalled();
   });
 });
--- sdk/react/src/markers/TextMarker/TextMarker.test.js
+++ sdk/react/src/markers/TextMarker/TextMarker.test.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import TestRenderer from 'react-test-renderer';
 
 import TextMarker from './TextMarker';
@@ -15,5 +16,37 @@ describe('TextMarker', () => {
     );
 
     expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('triggers `onSeek` callback every time when pressed if text is short', () => {
+    const onSeekMock = jest.fn();
+    const testRenderer = TestRenderer.create(
+      <TextMarker
+        text="A word"
+        onSeek={onSeekMock}
+      />,
+    );
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).toBeCalled();
+  });
+
+  it('triggers `onSeek` callback when pressed being expanded if text is long', () => {
+    const onSeekMock = jest.fn();
+    const testRenderer = TestRenderer.create(
+      <TextMarker
+        text="The text long enough to switch to collapsible state"
+        onSeek={onSeekMock}
+      />,
+    );
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).not.toBeCalled();
+
+    testRenderer.root.findByType(TouchableWithoutFeedback).props.onPress();
+
+    expect(onSeekMock).toBeCalled();
   });
 });
