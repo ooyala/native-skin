a82f5a0ab3badfe6075d1c04684a2a8aca5c32e5
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-05-10 15:35:15 +0300

    [PLAYER-5491] Added separate class to support live-related interaction with RCT bridge

---
+++ sdk/iOS/OoyalaSkinSDK/OOLiveVideoManager.h
@@ -0,0 +1,16 @@
+//
+//  OOLiveVideoManager.h
+//  OoyalaSkinSDK
+//
+//  Copyright © 2019 Ooyala, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OOLiveVideoManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdk/iOS/OoyalaSkinSDK/OOLiveVideoManager.m
@@ -0,0 +1,12 @@
+//
+//  OOLiveVideoManager.m
+//  OoyalaSkinSDK
+//
+//  Copyright © 2019 Ooyala, Inc. All rights reserved.
+//
+
+#import "OOLiveVideoManager.h"
+
+@implementation OOLiveVideoManager
+
+@end
