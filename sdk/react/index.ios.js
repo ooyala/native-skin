9a2f680c298b9a9fb77aadadc6be12e23f1bc03d
Danila Loginov <danila@loginov.rocks>
2019-06-24 16:58:59 +0300

    [PLAYER-5644] Make TODOs consistent, remove implemented.

--- sdk/react/index.ios.js
+++ sdk/react/index.ios.js
@@ -31,9 +31,8 @@ const styles = StyleSheet.create({
 });
 
 class OoyalaSkin extends Component {
-  // note/todo: some of these are more like props, expected to be over-ridden/updated
-  // by the native bridge, and others are used purely on the non-native side.
-  // consider using a leading underscore, or something?
+  // TODO: Some of these are more like props, expected to be over-ridden/updated by the native bridge, and others are
+  // used purely on the non-native side. Consider using a leading underscore, or something?
   state = {
     // states from react
     screenType: SCREEN_TYPES.LOADING_SCREEN,
@@ -90,7 +89,7 @@ class OoyalaSkin extends Component {
         });
       });
 
-    // TODO: Figure out how to add setAccessibilityFocus method from the ObjC side
+    // TODO: Figure out how to add setAccessibilityFocus method from the ObjC side.
     // AccessibilityInfo.setAccessibilityFocus(1);
   }
 
